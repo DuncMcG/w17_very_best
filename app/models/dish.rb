2f34bd0aa81cbf0f47bce27919076f9c049432bc
Raghu Betina <raghu@firstdraft.com>
2017-01-04 23:38:39 +0000

    Direct association between cuisines and dishes

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,6 +1,8 @@
 class Dish < ApplicationRecord
   # Direct associations
 
+  belongs_to :cuisine
+
   has_many   :menuitems,
              :class_name => "Entree",
              :dependent => :destroy
