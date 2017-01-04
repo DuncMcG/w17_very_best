339d6a0e54f1e37e132ae34f9cb217cc9b7aec77
Raghu Betina <raghu@firstdraft.com>
2017-01-04 23:38:39 +0000

    Direct association between entrees and bookmarkeds

--- app/models/bookmarked.rb
+++ app/models/bookmarked.rb
@@ -1,6 +1,8 @@
 class Bookmarked < ApplicationRecord
   # Direct associations
 
+  belongs_to :entree
+
   # Indirect associations
 
   # Validations
--- app/models/entree.rb
+++ app/models/entree.rb
@@ -1,6 +1,9 @@
 class Entree < ApplicationRecord
   # Direct associations
 
+  has_many   :bookmarkeds,
+             :dependent => :destroy
+
   belongs_to :dish
 
   belongs_to :venue
